d575ab688de035e3610efcd869b467c6bdd2f0e6
Tao <mrhaotao@gmail.com>
2024-01-07 02:28:31 +0800

    delete .idea

---
+++ flight-analysis-server/src/main/java/czu/bigdata/flightAnalysis/utils/MySparkSession.java
@@ -0,0 +1,34 @@
+package czu.bigdata.flightAnalysis.utils;
+
+import org.apache.spark.sql.SparkSession;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+import org.apache.spark.sql.SparkSession;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MySparkSession {
+
+    private static SparkSession sparkSession;
+
+    private static class SparkSessionHolder {
+        private static final SparkSession INSTANCE = createSparkSession();
+    }
+
+    public static SparkSession getSparkSession() {
+        return SparkSessionHolder.INSTANCE;
+    }
+
+    private static SparkSession createSparkSession() {
+        // 在这里创建 SparkSession 的逻辑
+        if (sparkSession == null) {
+            sparkSession = SparkSession.builder()
+                    .appName("YourAppName")
+                    .master("local[*]")  // 适配你的 Spark 环境
+                    .getOrCreate();
+        }
+        return sparkSession;
+    }
+}
